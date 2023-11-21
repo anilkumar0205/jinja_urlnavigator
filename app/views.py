ffea0d6f49359bc3036dd9e2261b7f528de1d421
anilkumar0205 <anilkallagunta0205@gmail.com>
2023-11-21 15:01:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sudheer(request):
+    return render(request, 'sudheer.html')
+
+def sudheer2(request):
+    return render(request, 'sudheer2.html')
